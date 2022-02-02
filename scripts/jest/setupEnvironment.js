c9e910e69750f8cc8988e227208f2189572e7630
王琳 <mag1c_shen_09@foxmail.com>
2022-02-02 16:50:07 +0800

    feat: 添加 develop dependencies, add reflect-metadata to jest setup

--- scripts/jest/setupEnvironment.js
+++ scripts/jest/setupEnvironment.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 
+require('reflect-metadata');
 const NODE_ENV = process.env.NODE_ENV;
 if (NODE_ENV !== 'development' && NODE_ENV !== 'production') {
   throw new Error('NODE_ENV must either be set to development or production.');
